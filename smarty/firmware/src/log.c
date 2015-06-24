5963e7ec61eaa3e3f2661e50dbd500e63b110fc4
Toth Daniel <danitoth91@gmail.com>
2015-06-24 09:31:22 +0200

    Log
    Első variáció működő log-ra.

--- smarty/firmware/src/log.c
+++ smarty/firmware/src/log.c
@@ -5,6 +5,7 @@
 
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #include "ch.h"
 #include "hal.h"
@@ -14,73 +15,34 @@
 #include "sdcard.h"
 
 #include "log.h"
+#include "meas.h"
+#include "logItems.h"
 
 #define LOG_WA_SIZE (2048)
-#define LOG_BUFFER_SIZE (100)
-#define LOG_MAX_IDLE_CNT (200 / 20)
-#define LOG_MAX_PERIOD (25000)
-
-struct logEntry
-{
-  uint32_t time;
-  int32_t igncnt;
-  int32_t injcnt;
-  int16_t injtime;
-  int16_t ignangle;
-  uint16_t rpm;
-  uint16_t ubat;
-  uint16_t cht;
-  uint16_t mat;
-  uint16_t cyt;
-  uint16_t trq;
-  uint16_t eff;
-  uint16_t fup;
-  uint16_t cls;
-  uint16_t bws;
-  uint16_t avg_bws;
-  uint16_t fuelcon;
-  uint16_t afr;
-  int32_t lon;
-  int32_t lat;
-  int16_t alt;
-  int16_t vel;
-  int8_t hdg;
-  uint8_t svs;
-  int16_t sta;
-  uint16_t pwm;
-  uint16_t bws_rpm;
-} __attribute__((packed));
-
-struct logEntry logBuffer[LOG_BUFFER_SIZE];
-int logReadPointer;
-int logWritePointer;
 
 enum logStates logState;
 FIL logFileObject;
 bool_t logStartRequest;
 bool_t logStopRequest;
 systime_t logStartTime;
-uint32_t logIdleCount;
 
-char logFileName[] = "test.log";
+char logFileName[] = "test.mega";
 
 static msg_t logThread(void *arg);
-static allapot;
-static allapot2;
+static uint16_t size = 0;
+static uint32_t logPeriod = 0;
+static uint32_t szam = 0;
 
 /*
  * Initializes log 
  */
 void logInit(void){
-  logReadPointer = 0;
-  logWritePointer = 0;
   logState = LOG_STOPPED;
   logStartRequest = FALSE;
   logStopRequest = FALSE;
   logStartTime = 0;
-  logIdleCount = 0;
-  allapot = 0;
-  allapot2 = 0;
+  logPeriod = 0;
+  size = 0;
 
   static WORKING_AREA(waLog, LOG_WA_SIZE);
   chThdCreateStatic(waLog, sizeof(waLog), LOWPRIO, logThread, NULL);
@@ -106,6 +68,8 @@ void twodigit(int n, char s[]){
  */
 void logStart(void){
   chSysLock();
+  logStartTime = chTimeNow();
+  logPeriod = 0;
   logStartRequest = TRUE;
   chSysUnlock();
 }
@@ -125,38 +89,22 @@ void logStop(void){
  * Main modue to be called from periodic task, handles buffering and flushing of logged data 
  */
 void logCalc(void){
-  int actualReadPointer;
-  chSysLock();
-  actualReadPointer = logReadPointer;
-  chSysUnlock();
-  systime_t logTime;
+  size = LOG_ITEMS_NUM;
   
   if (logGetState() == LOG_RUNNING){
-        allapot = 1;
-/*     if((   actualIgnCount != logLastIgnCount \
-        && actualLastPeriod < LOG_MAX_PERIOD)          \
-        || logIdleCount > LOG_MAX_IDLE_CNT){
-                                                                       // igncount = -2
-        if(actualIgnCount > logLastIgnCount){               // actualIgnCount = ecGetIgnCount() > logLastIgnCount = -2;
-            ecGetLastIgnStartTime() - logStartTime;         // ecGetLastIgnStartTime() = gyujtásonként a jelenlegi idő; logStartTime = logolás után a jelenlegi idő
-        }
-        else{
-            chTimeNow() - logStartTime;                     // jelenlegi idő - logolások utáni jelenlegi idő
-        }
 
-      logBuffer[logWritePointer].time = (actualIgnCount > logLastIgnCount ? ecGetLastIgnStartTime() : chTimeNow()); //- logStartTime;
-      logBuffer[logWritePointer].igncnt = actualIgnCount;*/
-      
-      chSysLock();
-      logWritePointer = logWritePointer < LOG_BUFFER_SIZE - 1 ? logWritePointer + 1 : 0;
-      chSysUnlock();
-      /*logLastIgnCount = actualIgnCount;*/
-      logIdleCount = 0;
-
-    }
-    else {
-      logIdleCount++;
-    }
+    logitems[NUM].value = logPeriod;
+    logitems[TIME].value = chTimeNow() - logStartTime;
+    logitems[UBAT].value = measGetValue(MEAS_UBAT);
+    logitems[BRAKE_P1].value = measGetValue(MEAS_BRAKE_PRESSURE1);
+    logitems[BRAKE_P2].value = measGetValue(MEAS_BRAKE_PRESSURE2);
+    logitems[FUT_SEN1].value = measGetValue(MEAS_SEN2);
+    logitems[FUT_SEN2].value = measGetValue(MEAS_SEN3);
+    logitems[FUT_SEN3].value = measGetValue(MEAS_SEN4);
+    logitems[FUT_SEN4].value = measGetValue(MEAS_SEN5);
+    logitems[CURR1].value = measGetValue_2(MEAS2_CURR1);
+    logitems[THROTTLE].value = measGetValue_2(MEAS2_THROTTLE);
+  }
 }
 
 /*
@@ -165,14 +113,9 @@ void logCalc(void){
 static msg_t logThread(void *arg) {
   bool_t isStartRequest;
   bool_t isStopRequest;
-  int actualWritePointer;
-  char teststring[10];
-  teststring[0] = "H";
-  teststring[1] = "e";
-  teststring[2] = "l";
+  int i;
   
   FRESULT err;
-  UINT bytesWritten;
   
   (void)arg;
   chRegSetThreadName("logTask");
@@ -183,7 +126,6 @@ static msg_t logThread(void *arg) {
     isStopRequest = logStopRequest;
     logStartRequest = FALSE;
     logStopRequest = FALSE;
-    actualWritePointer = logWritePointer;
     chSysUnlock();
 
     switch(logState){
@@ -193,7 +135,6 @@ static msg_t logThread(void *arg) {
           if (sdcardIsMounted()) {
             err = f_open(&logFileObject, logFileName, FA_WRITE | FA_OPEN_ALWAYS);
             if (err == FR_OK) {
-              allapot = 50;
               chSysLock();
               logStartTime = chTimeNow();
               logState = LOG_RUNNING;
@@ -208,25 +149,57 @@ static msg_t logThread(void *arg) {
           logState = LOG_STOPPING;
           chSysUnlock();
         }
-        while(actualWritePointer != logReadPointer)
+
+        else if (logPeriod == 0)
         {
-          allapot = 256;
-          err = f_write(&logFileObject, teststring, sizeof(teststring), &bytesWritten);
-          if (err != FR_OK || bytesWritten != sizeof(struct logEntry)){
-            isStopRequest = TRUE;
-            break;
+          for (i = 0; i < LOG_ITEMS_NUM; ++i)
+          {
+            err = f_printf (&logFileObject, "%s", logitems[i].name);
+            if (err == EOF)
+            {
+              isStopRequest = TRUE;
+              break;
+            }
           }
-          err = f_sync(&logFileObject);
-          if (err != FR_OK){
-            isStopRequest = TRUE;
+          err = f_puts ("\r\n", &logFileObject);
+
+          for (i = 0; i < LOG_ITEMS_NUM; ++i)
+          {
+            err = f_printf (&logFileObject, "%s", logitems[i].convert);
           }
-          chSysLock();
-          logReadPointer = logReadPointer < LOG_BUFFER_SIZE - 1 ? logReadPointer + 1 : 0;
-          chSysUnlock();
+          err = f_puts ("\r\n", &logFileObject);
+          szam = err;
+          logPeriod += 1;
+        }         
+        else if (logPeriod > 0)
+        {
+          for (i = 0; i < LOG_ITEMS_NUM; ++i)
+          {
+            err = f_printf (&logFileObject, "%d;", logitems[i].value);
+            //sprintf(logString, "%s%d;", logString, logitems[i].value);
+          }
+          err = f_puts ("\r\n", &logFileObject);
+
+          /*sprintf(logString, "%d;%d;%d;%d;%d;%d;%d;%d;%d\r\n", \
+             measGetValue(MEAS_UBAT),                       \
+             measGetValue(MEAS_BRAKE_PRESSURE1),            \
+             measGetValue(MEAS_BRAKE_PRESSURE2),            \
+             measGetValue(MEAS_SEN2),                       \
+             measGetValue(MEAS_SEN3),                       \
+             measGetValue(MEAS_SEN4),                       \
+             measGetValue(MEAS_SEN5),                       \
+             measGetValue_2(MEAS2_CURR1),                   \
+             measGetValue_2(MEAS2_THROTTLE)                 \
+          );*/
+          logPeriod += 1;
         }
+          
+        err = f_sync(&logFileObject);
+        if (err != FR_OK){
+          isStopRequest = TRUE;
+        }
+
         if(isStopRequest){
-          logReadPointer = 0;
-          logWritePointer = 0;
           f_close(&logFileObject);
           sdcardUMount();
           chSysLock();
@@ -244,7 +217,7 @@ static msg_t logThread(void *arg) {
       default:
         break;
     }
-    chThdSleepMilliseconds(500);
+    chThdSleepMilliseconds(50);
   }
   return 0; /* Never executed.*/
 }
@@ -291,6 +264,7 @@ systime_t logGetTime(void){
 
 void cmd_testlog(BaseSequentialStream *chp, int argc, char *argv[]) {
   enum logStates state;
+  uint32_t old_logPeriod = 0;
   (void)argc;
   (void)argv;
   chprintf(chp, "\x1B\x63");
@@ -298,19 +272,21 @@ void cmd_testlog(BaseSequentialStream *chp, int argc, char *argv[]) {
   logStart();
   chprintf(chp, "Logging starting...\r\n");
   while (chnGetTimeout((BaseChannel *)chp, TIME_IMMEDIATE) == Q_TIMEOUT) {
-    state = logGetState();
-    chprintf(chp, "\x1B[%d;%dH", 0, 0);
-    chprintf(chp, "Filename = %s Logtime = %D    \r\n", logFileName, chTimeNow() - logStartTime);
-    chprintf(chp, "logState = %s \r\n", 
-             state == LOG_RUNNING ? "LOG_RUNNING" : \
-             state == LOG_STOPPED ? "LOG_STOPPED" : \
-             state == LOG_STOPPING ? "LOG_STOPPING" : "LOG_DOWNLOADING");
-    if (state == LOG_RUNNING){
-      chprintf(chp, "ReadPointer = %D WritePointer = %D    \r\n", logReadPointer, logWritePointer);
+    if(old_logPeriod != logPeriod){
+      chprintf(chp, "\x1B[%d;%dH", 0, 0);
+      state = logGetState();
+      chprintf(chp, "\x1B[%d;%dH", 0, 0);
+      chprintf(chp, "Filename = %s Logtime = %D    \r\n", logFileName, chTimeNow() - logStartTime);
+      chprintf(chp, "logState = %s \r\n", 
+               state == LOG_RUNNING ? "LOG_RUNNING" : \
+               state == LOG_STOPPED ? "LOG_STOPPED" : \
+               state == LOG_STOPPING ? "LOG_STOPPING" : "LOG_DOWNLOADING");
+      chprintf(chp, "size: %d\r\n", size);
+      chprintf(chp, "logPeriod: %d\r\n", logPeriod);
+      chprintf(chp, "szam: %d\r\n", szam);
+      old_logPeriod = logPeriod;
     }
-    chprintf(chp, "allapot: %15d", allapot);
-    chprintf(chp, "allapot2: %15d", allapot2);
-    chThdSleepMilliseconds(200);
+    chThdSleepMilliseconds(10);
   }
   logStop();
   chprintf(chp, "\r\nLogging stopped\r\n");
